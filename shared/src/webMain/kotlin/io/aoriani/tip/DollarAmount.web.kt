6e06f9d3f344781f01e0911f7bdfa3a322d2db0c
André Oriani <aoriani@gmail.com>
2026-05-19 16:59:49 -0700

    Fixing Dollar testing.

--- shared/src/webMain/kotlin/io/aoriani/tip/DollarAmount.web.kt
+++ shared/src/webMain/kotlin/io/aoriani/tip/DollarAmount.web.kt
@@ -5,6 +5,8 @@ import kotlin.js.JsModule
 
 @OptIn(ExperimentalWasmJsInterop::class)
 @JsModule("big.js")
+@Suppress("UNRESOLVED_REFERENCE")
+@kotlin.js.JsNonModule
 private external class Big {
     constructor(value: String)
     fun plus(other: Big): Big
